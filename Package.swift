df159353e4b4344b14a5683e45c1ffb5b26127e2
Hakan <hakan.kumdakci@mobven.com>
2026-03-02 16:27:35 +0300

    fixed misNaming in package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "MbAsyncNetworkingSpm",
+            name: "MBAsyncNetworking",
             targets: ["MBAsyncNetworking"]
         ),
     ],
